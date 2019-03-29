cff340bcde23eecbf867ce9f4c0c1f992637c1fa
FajsiEx <fajsiexofficial@gmail.com>
2019-03-29 14:46:31 +0100

    update / you can now use an array of prefixes

--- handlers/message.js
+++ handlers/message.js
@@ -3,16 +3,28 @@ const commandHandler = require("./command").handler;
 const handleDataCheck = require("../checks/handleData").check;
 
 module.exports = {
-    handler: (handleData)=>{
+    handler: function(handleData) {
         if (handleDataCheck(handleData)) {
             console.log("[HANDLER:MSG] handleData check failed. Returning false.");
             return false;
         }
+        
+        if (handleData.msg.author.bot) {
+            console.log("[HANDLER:MSG] Bot message ignored.".debug);
+        }
 
-        if (handleData.msg.content.startsWith(CONFIG.DISCORD.PREFIXES[0])) {
+        if (module.exports.stringStartsWithPrefix(handleData.msg.content)) {
             console.log("[HANDLER:MSG] Passing off to COMMAND handler");
             commandHandler(handleData);
             return true;
         }
+    },
+
+    stringStartsWithPrefix: function(stringToBeChecked) {
+        let containsPrefix = CONFIG.DISCORD.PREFIXES.filter(prefix => {
+            return stringToBeChecked.startsWith(prefix);
+        })[0];
+
+        return containsPrefix;
     }
 };
