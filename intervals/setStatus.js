451abc959240563aaa775e8682f3212d30a1847c
FajsiEx <fajsiexofficial@gmail.com>
2019-04-13 10:02:55 +0200

    update / setStatus - changed to playing stat & time has leading zeroes

--- intervals/setStatus.js
+++ intervals/setStatus.js
@@ -4,10 +4,19 @@ module.exports = {
     interval: function(dClient) {
         console.log("[INTERVAL:SET_STATUS] INTERVAL Set status".interval);
 
-        let activityString = `${dClient.guilds.size} guilds | ${CONFIG.BOT.BUILD_INFO.BUILD_STRING} | ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`;
+        let activityString = `with ${dClient.guilds.size} guilds | ${CONFIG.BOT.BUILD_INFO.BUILD_STRING} | ${module.exports.getTimeString()}`;
 
-        dClient.user.setActivity(activityString, { type: "LISTENING" }).then(()=>{
+        dClient.user.setActivity(activityString, { type: "PLAYING" }).then(()=>{
             console.log(`[INTERVAL:SET_STATUS] DONE Set status to [${activityString}]`.success);
         });
+    },
+    
+    getTimeString: function() {
+        let dt = new Date();
+        let hours = (dt.getHours() < 10 ? '0' : '') + dt.getHours();
+        let minutes = (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+        let seconds = (dt.getSeconds() < 10 ? '0' : '') + dt.getSeconds();
+
+        return `${hours}:${minutes}:${seconds}`
     }
 };
