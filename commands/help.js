df77657ba933db5fac9a248f56cae6c3b3550f7a
FajsiEx <fajsiexofficial@gmail.com>
2019-04-13 22:55:59 +0200

    update / help has more info

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const CONFIG = require("../modules/config");
 
 module.exports = {
-    handler: (handleData)=>{
+    handler: (handleData) => {
         let msg = handleData.msg;
         msg.channel.send({
             embed: {
@@ -9,7 +9,12 @@ module.exports = {
                 "color": CONFIG.EMBED.COLORS.INFO,
                 "description": `
                     Tea-bot Project
+                    by FajsiEx
                     Build ${CONFIG.BOT.BUILD_INFO.BUILD_STRING}
+                    Licensed under MIT license.
+
+                    [Website (tea-bot.ml)](https://tea-bot.ml)
+                    [GitHub (FajsiEx/tea-bot)](https://github.com/FajsiEx/tea-bot)
                 `,
                 "footer": CONFIG.EMBED.FOOTER(handleData)
             }
