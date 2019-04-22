dda14112687e45d68b0e9fadb509c5c6582d4d04
FajsiEx <fajsiexofficial@gmail.com>
2019-04-22 15:57:42 +0200

    > converted messageHandler to return a promise

--- handlers/message.js
+++ handlers/message.js
@@ -4,45 +4,50 @@ const handleDataCheck = require("../checks/handleData").check;
 const dbInt = require("../db/interface");
 
 module.exports = {
-    handler: function(handleData) {
-        if (handleDataCheck(handleData)) {
-            console.log("[HANDLER:MSG] handleData check failed. Returning false.");
-            return false;
-        }
-        
-        if (handleData.msg.author.bot) {
-            console.log("[HANDLER:MSG] Bot message ignored.".debug);
-            return false;
-        }
-
-        let guildId = handleData.msg.guild.id;
-        dbInt.getGuildDoc(guildId).then((doc)=>{
-            console.log("[COMMAND:DEV:INTWRITE] DEBUG Got guild doc".debug);
-
-            if (typeof(doc.messageCount) != 'object') {
-                doc.messageCount = {};
+    handler: function (handleData) {
+        return new Promise((resolve, reject) => {
+            if (handleDataCheck(handleData)) {
+                console.log("[HANDLER:MSG] handleData check failed. Returning false.");
+                reject("Failed handleData check");
             }
 
-            if(typeof(doc.messageCount[handleData.msg.author.id]) != 'number') {
-                doc.messageCount[handleData.msg.author.id] = 0;
-            } 
+            if (handleData.msg.author.bot) {
+                console.log("[HANDLER:MSG] Bot message ignored.".debug);
+                resolve(2); // 2 = ignored bot message
+            }
 
-            doc.messageCount[handleData.msg.author.id]++;
+            let guildId = handleData.msg.guild.id;
+            dbInt.getGuildDoc(guildId).then((doc) => {
+                console.log("[COMMAND:DEV:INTWRITE] DEBUG Got guild doc".debug);
 
-            console.log(`[HANDLER:MSG] Incrementing msgcount for [${guildId}:${handleData.msg.author.id}] to ${doc.messageCount[handleData.msg.author.id]}`.debug);
-
-            dbInt.setGuildDoc(guildId, doc).then(()=>{ // After the doc is saved
-                let commandPrefix = module.exports.stringStartsWithPrefix(handleData.msg.content);
-                if (commandPrefix) {
-                    console.log("[HANDLER:MSG] Passing off to COMMAND handler");
-                    commandHandler(handleData, commandPrefix);
-                    return true;
+                if (typeof (doc.messageCount) != 'object') {
+                    doc.messageCount = {};
                 }
+
+                if (typeof (doc.messageCount[handleData.msg.author.id]) != 'number') {
+                    doc.messageCount[handleData.msg.author.id] = 0;
+                }
+
+                doc.messageCount[handleData.msg.author.id]++;
+
+                console.log(`[HANDLER:MSG] Incrementing msgcount for [${guildId}:${handleData.msg.author.id}] to ${doc.messageCount[handleData.msg.author.id]}`.debug);
+
+                dbInt.setGuildDoc(guildId, doc).then(() => { // After the doc is saved
+                    let commandPrefix = module.exports.stringStartsWithPrefix(handleData.msg.content);
+                    if (commandPrefix) {
+                        console.log("[HANDLER:MSG] Passing off to COMMAND handler");
+                        commandHandler(handleData, commandPrefix).then((status)=>{
+                            resolve(1); // 1 = handled command
+                        }).catch((e)=>{
+                            reject("commandHandler rejected it's promise: " + e);
+                        });
+                    }
+                });
             });
         });
     },
 
-    stringStartsWithPrefix: function(stringToBeChecked) {
+    stringStartsWithPrefix: function (stringToBeChecked) {
         let containsPrefix = CONFIG.DISCORD.PREFIXES.filter(prefix => {
             return stringToBeChecked.startsWith(prefix);
         })[0];
