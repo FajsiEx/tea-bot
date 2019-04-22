dae6613775646e1afc04af0796cdf0dd76f3d582
FajsiEx <fajsiexofficial@gmail.com>
2019-04-22 16:47:31 +0200

    > converted cpp:congrats to use promises

--- commands/copypaste/congrats.js
+++ commands/copypaste/congrats.js
@@ -1,17 +1,26 @@
 const CONFIG = require("../../modules/config");
 
 module.exports = {
-    handler: (handleData)=>{
-        let msg = handleData.msg;
-        msg.channel.send({
-            "embed": {
-                "title": "Congrats :D",
-                "color": CONFIG.EMBED.COLORS.INFO,
-                "description": `
-                🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉
-                `,
-                "footer": CONFIG.EMBED.FOOTER(handleData)
+    handler: (handleData) => {
+        return new Promise((resolve, reject) => {
+            let msg = handleData.msg;
+            if (!msg) {
+                reject("Message is false");
             }
+            msg.channel.send({
+                "embed": {
+                    "title": "Congrats :D",
+                    "color": CONFIG.EMBED.COLORS.INFO,
+                    "description": `
+                        🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉
+                    `,
+                    "footer": CONFIG.EMBED.FOOTER(handleData)
+                }
+            }).then(()=>{
+                resolve(0);
+            }).catch((e)=>{
+                reject("Failed to send a message: " + e);
+            });
         });
     }
-}
+};
