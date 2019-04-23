fd13d42307be8fe67fc240c4e7f2b5701fca2355
FajsiEx <fajsiexofficial@gmail.com>
2019-04-23 10:22:48 +0200

    > converted all of dev/db to use promises

--- commands/dev/db/deletegd.js
+++ commands/dev/db/deletegd.js
@@ -2,49 +2,58 @@ const CONFIG = require("../../../modules/config");
 const dbBridge = require("../../../db/bridge");
 
 module.exports = {
-    handler: (handleData)=>{
-        let msg = handleData.msg;
+    handler: (handleData) => {
+        return new Promise((resolve, reject) => {
+            let msg = handleData.msg;
 
-        let guildId = false;
+            let guildId = false;
 
-        if (msg.guild) {
-            guildId = msg.guild.id;
-        }
+            if (msg.guild) {
+                guildId = msg.guild.id;
+            }
 
-        console.log("[COMMAND:DEV:GGD] DEBUG ARG1: " + msg.content.split(" ")[1]);
-        let commandArg_guildId = parseInt(msg.content.split(" ")[1])
-        if (commandArg_guildId) {
-            guildId = commandArg_guildId;
-        }
+            console.log("[COMMAND:DEV:GGD] DEBUG ARG1: " + msg.content.split(" ")[1]);
+            let commandArg_guildId = parseInt(msg.content.split(" ")[1])
+            if (commandArg_guildId) {
+                guildId = commandArg_guildId;
+            }
 
-        if (!guildId) { // If the msg isn't in a guild (to get id from) and trhe user hasn't specified an id in the params, please fuck off
-            msg.channel.send({
-                "embed": {
-                    "title": "Delete guild document",
-                    "color": CONFIG.EMBED.COLORS.FAIL,
-                    "description": `
-                        Could not get guild id from the message and no id was specified.
-                    `,
-                    "footer": CONFIG.EMBED.FOOTER(handleData)
-                }
+            if (!guildId) { // If the msg isn't in a guild (to get id from) and the user hasn't specified an id in the params, please fuck off
+                msg.channel.send({
+                    "embed": {
+                        "title": "Delete guild document",
+                        "color": CONFIG.EMBED.COLORS.FAIL,
+                        "description": `
+                            Could not get guild id from the message and no id was specified.
+                        `,
+                        "footer": CONFIG.EMBED.FOOTER(handleData)
+                    }
+                }).then(()=>{
+                    resolve(1);
+                }).catch((e)=>{
+                    reject("Failed to send invalid parameter message: " + e);
+                });
+            }
+
+            dbBridge.deleteGuildDocument(guildId).then(() => {
+                msg.channel.send({
+                    "embed": {
+                        "title": "Delete guild document",
+                        "color": CONFIG.EMBED.COLORS.SUCCESS,
+                        "description": `
+                            Deleted guild document.
+                            Guild ID: ${guildId}
+                        `,
+                        "footer": CONFIG.EMBED.FOOTER(handleData)
+                    }
+                }).then(()=>{
+                    resolve(0);
+                }).catch((e)=>{
+                    reject("Failed to send a success message: " + e);
+                });
+            }).catch((e)=>{
+                reject("Failed to deleteGuildDocument: " + e);
             });
-            return false;
-        }
-
-        dbBridge.deleteGuildDocument(guildId).then(()=>{
-            msg.channel.send({
-                "embed": {
-                    "title": "Delete guild document",
-                    "color": CONFIG.EMBED.COLORS.SUCCESS,
-                    "description": `
-                        Deleted guild document.
-                        Guild ID: ${guildId}
-                    `,
-                    "footer": CONFIG.EMBED.FOOTER(handleData)
-                }
-            });
-        });
-
-        
-    }
+        }); // End of promise
+    } // End of handler
 };
--- commands/dev/db/testwrite.js
+++ commands/dev/db/testwrite.js
@@ -2,52 +2,63 @@ const CONFIG = require("../../../modules/config");
 const dbBridge = require("../../../db/bridge");
 
 module.exports = {
-    handler: (handleData)=>{
-        let msg = handleData.msg;
+    handler: (handleData) => {
+        return new Promise((resolve, reject) => {
+            let msg = handleData.msg;
 
-        let guildId = false;
+            let guildId = false;
 
-        if (msg.guild) {
-            guildId = msg.guild.id;
-        }
+            if (msg.guild) {
+                guildId = msg.guild.id;
+            }
 
-        if (!guildId) { // If the msg isn't in a guild (to get id from) please fuck off
-            msg.channel.send({
-                "embed": {
-                    "title": "Write guild document",
-                    "color": CONFIG.EMBED.COLORS.FAIL,
-                    "description": `
-                        Could not get guild id from the message.
-                    `,
-                    "footer": CONFIG.EMBED.FOOTER(handleData)
-                }
-            });
-            return false;
-        }
-
-        dbBridge.getGuildDocument(guildId).then((doc)=>{
-            console.log("[COMMAND:DEV:TESTWRITE] DEBUG Got guild doc".debug);
-
-            doc.testValue = `
-            1273, down to Rockefeller Street
-            Life is marchin' on do you feel that
-            `; // Just some random test value
-
-            dbBridge.writeGuildDocument(guildId, doc).then(()=>{
+            if (!guildId) { // If the msg isn't in a guild (to get id from) please fuck off
                 msg.channel.send({
                     "embed": {
-                        "title": "Test write of guild document",
-                        "color": CONFIG.EMBED.COLORS.SUCCESS,
+                        "title": "Write guild document",
+                        "color": CONFIG.EMBED.COLORS.FAIL,
                         "description": `
-                            Done.
-                            Guild ID: ${guildId}
-                        `,
+                        Could not get guild id from the message.
+                    `,
                         "footer": CONFIG.EMBED.FOOTER(handleData)
                     }
+                }).then(()=>{
+                    resolve(1);
+                }).catch((e)=>{
+                    reject("Failed to send invalid parameter message: " + e);
                 });
-            }); // and write the doc
-        });
+            }
 
-        
-    }
+            dbBridge.getGuildDocument(guildId).then((doc) => {
+                console.log("[COMMAND:DEV:TESTWRITE] DEBUG Got guild doc".debug);
+
+                doc.testValue = `
+                    1273, down to Rockefeller Street
+                    Life is marchin' on do you feel that
+                `; // Just some random test value
+
+                dbBridge.writeGuildDocument(guildId, doc).then(() => {
+                    msg.channel.send({
+                        "embed": {
+                            "title": "Test write of guild document",
+                            "color": CONFIG.EMBED.COLORS.SUCCESS,
+                            "description": `
+                                Done.
+                                Guild ID: ${guildId}
+                            `,
+                            "footer": CONFIG.EMBED.FOOTER(handleData)
+                        }
+                    }).then(()=>{
+                        resolve(0);
+                    }).catch((e)=>{
+                        reject("Failed to send a success message: " + e);
+                    });
+                }).catch((e)=>{
+                    reject("Failed to writeGuildDocument: " + e);
+                });
+            }).catch((e)=>{
+                reject("Failed to getGuildDocument: " + e);
+            });
+        }); // End of promise
+    } // End of handler
 };
