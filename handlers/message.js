612b88bf46a69ac5910e2cfc4fc13b5505d2929d
FajsiEx <fajsiexofficial@gmail.com>
2019-04-25 18:32:09 +0200

    > added returns to resolve/reject in msgHandler

--- handlers/message.js
+++ handlers/message.js
@@ -8,17 +8,52 @@ module.exports = {
         return new Promise((resolve, reject) => {
             if (handleDataCheck(handleData)) {
                 console.log("[HANDLER:MSG] handleData check failed. Returning false.");
-                reject("Failed handleData check");
+                return reject("Failed handleData check");
             }
 
             if (handleData.msg.author.bot) {
                 console.log("[HANDLER:MSG] Bot message ignored.".debug);
-                resolve(2); // 2 = ignored bot message
+                return resolve(2); // 2 = ignored bot message
             }
 
-            let guildId = handleData.msg.guild.id;
+            let commandPrefix = module.exports.stringStartsWithPrefix(handleData.msg.content);
+            if (commandPrefix) {
+                console.log("[HANDLER:MSG] Passing off to COMMAND handler");
+                commandHandler(handleData, commandPrefix).then((status) => {
+                    module.exports.incrementMessageCount(handleData).then(()=>{
+                        return resolve(1); // 1 = handled command
+                    }).catch((e)=>{
+                        return reject("Increment message count failed: " + e);
+                    });                    
+                }).catch((e) => {
+                    return reject("commandHandler rejected it's promise: " + e);
+                });
+            } else {
+                module.exports.incrementMessageCount(handleData).then(()=>{
+                    return resolve(0); // 0 = handled msg without command
+                }).catch((e)=>{
+                    return reject("Increment message count failed: " + e);
+                });
+            }
+        });
+    },
+
+    stringStartsWithPrefix: function (stringToBeChecked) {
+        let containsPrefix = CONFIG.DISCORD.PREFIXES.filter(prefix => {
+            return stringToBeChecked.startsWith(prefix);
+        })[0];
+
+        return containsPrefix;
+    },
+
+    incrementMessageCount: function (handleData) {
+        return new Promise((resolve, reject) => {
+            guildId = handleData.msg.guild.id;
+
+            console.log("[COMMAND:DEV:MESSAGEINCREMENT] DEBUG Called".debug);
+
             dbInt.getGuildDoc(guildId).then((doc) => {
-                console.log("[COMMAND:DEV:INTWRITE] DEBUG Got guild doc".debug);
+                console.log("[COMMAND:DEV:MESSAGEINCREMENT] DEBUG Got guild doc".debug);
 
                 if (typeof (doc.messageCount) != 'object') {
                     doc.messageCount = {};
@@ -32,28 +67,15 @@ module.exports = {
 
                 console.log(`[HANDLER:MSG] Incrementing message count for [${guildId}:${handleData.msg.author.id}] to ${doc.messageCount[handleData.msg.author.id]}`.debug);
 
-                dbInt.setGuildDoc(guildId, doc).then(() => { // After the doc is saved
-                    let commandPrefix = module.exports.stringStartsWithPrefix(handleData.msg.content);
-                    if (commandPrefix) {
-                        console.log("[HANDLER:MSG] Passing off to COMMAND handler");
-                        commandHandler(handleData, commandPrefix).then((status)=>{
-                            resolve(1); // 1 = handled command
-                        }).catch((e)=>{
-                            reject("commandHandler rejected it's promise: " + e);
-                        });
-                    }else{
-                        resolve(0); // 0 = handled message without command
-                    }
+                dbInt.setGuildDoc(guildId, doc).then(() => {
+                    console.log("[COMMAND:DEV:MESSAGEINCREMENT] DEBUG Set guild doc. done.".debug);
+                    return resolve(0);
+                }).catch((e)=>{
+                    return reject("Failed to setGuildDoc on increment: " + e);
                 });
+            }).catch((e)=>{
+                return reject("Failed to getGuildDoc on increment: " + e);
             });
         });
-    },
-
-    stringStartsWithPrefix: function (stringToBeChecked) {
-        let containsPrefix = CONFIG.DISCORD.PREFIXES.filter(prefix => {
-            return stringToBeChecked.startsWith(prefix);
-        })[0];
-
-        return containsPrefix;
     }
 };
