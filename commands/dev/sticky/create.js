5ef7c8805f7d3c005b171e829e2f8dcbfcf5fe94
FajsiEx <fajsiexofficial@gmail.com>
2019-05-05 22:55:50 +0200

    > converted all dev/sticky commands to use promises

--- commands/dev/sticky/create.js
+++ commands/dev/sticky/create.js
@@ -1,18 +1,21 @@
-
 const stickyController = require("../../../sticky/stickyController");
 
 module.exports = {
-    handler: (handleData)=>{
-        let msg = handleData.msg;
+    handler: (handleData) => {
+        return new Promise((resolve, reject) => {
+            let msg = handleData.msg;
 
-        stickyController.createStickyPost({
-            guildId: msg.guild.id,
-            type: "time",
-            channel: msg.channel
-        }).then(()=>{
-            console.log("[COMMAND:DEV:STICKYCREATE] Resolved");
-        }).catch((e)=>{
-            console.log(`[COMMAND:DEV:STICKYCREATE] createStickyPost has rejected it's promise: ${e}`.error);
-        });
-    }
+            stickyController.createStickyPost({
+                guildId: msg.guild.id,
+                type: "time",
+                channel: msg.channel
+            }).then(() => {
+                console.log("[COMMAND:DEV:STICKYCREATE] Resolved");
+                return resolve(0);
+            }).catch((e) => {
+                console.log(`[COMMAND:DEV:STICKYCREATE] createStickyPost has rejected it's promise: ${e}`.error);
+                return reject(`createStickyPost has rejected it's promise: ${e}`);
+            });
+        }); // End of promise
+    } // End of handler
 };
--- commands/dev/sticky/deleteall.js
+++ commands/dev/sticky/deleteall.js
@@ -1,14 +1,17 @@
-
 const stickyController = require("../../../sticky/stickyController");
 
 module.exports = {
-    handler: (handleData)=>{
-        let msg = handleData.msg;
+    handler: (handleData) => {
+        return new Promise((resolve, reject) => {
+            let msg = handleData.msg;
 
-        stickyController.deleteAllStickyMessagesFromChannel(msg.channel.id).then(()=>{
-            console.log("[COMMAND:DEV:STICKYDELALL] Resolved");
-        }).catch((e)=>{
-            console.log(`[COMMAND:DEV:STICKYDELALL] deleteAllStickyMessagesFromChannel has rejected it's promise: ${e}`.error);
-        });
-    }
+            stickyController.deleteAllStickyMessagesFromChannel(msg.channel.id).then(() => {
+                console.log("[COMMAND:DEV:STICKYDELALL] Resolved");
+                return resolve(0);
+            }).catch((e) => {
+                console.log(`[COMMAND:DEV:STICKYDELALL] deleteAllStickyMessagesFromChannel has rejected it's promise: ${e}`.error);
+                return reject(`deleteAllStickyMessagesFromChannel has rejected it's promise: ${e}`);
+            });
+        }); // End of promise
+    } // End of handler
 };
