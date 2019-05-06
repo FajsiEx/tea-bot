a8ea5e53ead7508003311307d119f7e105b1131f
FajsiEx <fajsiexofficial@gmail.com>
2019-05-06 21:34:01 +0200

    > changed dev/send to use promises

--- commands/dev/send.js
+++ commands/dev/send.js
@@ -8,25 +8,33 @@
 const CONFIG = require("../../modules/config");
 
 module.exports = {
-    handler: (handleData)=>{
-        let msg = handleData.msg;
+    handler: (handleData) => {
+        return new Promise((resolve, reject) => {
+            let msg = handleData.msg;
 
-        let command = msg.content.split(" ")[0];
-        let channelId = msg.content.split(" ")[1];
-        let sendMsg = msg.content.slice(command.length + channelId.length + 2);
+            let command = msg.content.split(" ")[0];
+            let channelId = msg.content.split(" ")[1];
+            let sendMsg = msg.content.slice(command.length + channelId.length + 2);
 
-        msg.channel.send({
-            "embed": {
-                "title": "Send",
-                "color": CONFIG.EMBED.COLORS.SUCCESS,
-                "description": `
+            msg.channel.send({
+                "embed": {
+                    "title": "Send",
+                    "color": CONFIG.EMBED.COLORS.SUCCESS,
+                    "description": `
                     The following message will be sent to \`${channelId}\`
                     \`\`\`${sendMsg}\`\`\`
                 `,
-                "footer": CONFIG.EMBED.FOOTER(handleData)
-            }
-        });
+                    "footer": CONFIG.EMBED.FOOTER(handleData)
+                }
+            }).catch((e)=>{
+                return reject("Failed to send response: " + e);
+            });
 
-        handleData.dClient.channels.get(channelId).send(sendMsg);
+            handleData.dClient.channels.get(channelId).send(sendMsg).then(()=>{
+                return resolve(0);
+            }).catch((e)=>{
+                return reject("Failed to send message to the specified channel: " + e);
+            });
+        });
     }
 };
