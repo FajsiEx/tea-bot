821301e7ba0a2bd583d9a11b5178bf9b74274b36
FajsiEx <fajsiexofficial@gmail.com>
2019-05-11 22:34:55 +0200

    > converted !ping to use promises. Closes #4

--- commands/ping.js
+++ commands/ping.js
@@ -1,17 +1,23 @@
 const CONFIG = require("../modules/config");
 
 module.exports = {
-    handler: (handleData)=>{
-        let msg = handleData.msg;
-        msg.channel.send({
-            "embed": {
-                "title": "Ping",
-                "color": CONFIG.EMBED.COLORS.INFO,
-                "description": `
+    handler: (handleData) => {
+        return new Promise((resolve, reject) => {
+            let msg = handleData.msg;
+            msg.channel.send({
+                "embed": {
+                    "title": "Ping",
+                    "color": CONFIG.EMBED.COLORS.INFO,
+                    "description": `
                     Server message latency: ${new Date().getTime() - msg.createdTimestamp} ms
                 `,
-                "footer": CONFIG.EMBED.FOOTER(handleData)
-            }
+                    "footer": CONFIG.EMBED.FOOTER(handleData)
+                }
+            }).then(()=>{
+                return resolve(0);
+            }).catch((e)=>{
+                return reject("Failed sending message: " + e)''
+            });
         });
     }
 };
