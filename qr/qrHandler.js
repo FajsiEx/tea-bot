639d2530c07e26e51d8751d320cc0ee9d06bf82d
FajsiEx <fajsiexofficial@gmail.com>
2019-05-12 13:59:59 +0200

    + basics of qrHandler

--- qr/qrHandler.js
+++ qr/qrHandler.js
@@ -1,6 +1,11 @@
 
+const qrData = require("./qrData");
+
 module.exports = {
-    handler: (handleData)=>{
-        
+    handler: (handleData, usedCommand)=>{
+        return new Promise((resolve, reject) => {
+            console.log("[QR:HANDLER] Called " + usedCommand);
+            resolve(0);
+        });
     }
 };
