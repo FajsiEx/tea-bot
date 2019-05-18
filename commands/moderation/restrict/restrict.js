38e6d5ba093d2425d6431619f74c5faae085df74
FajsiEx <fajsiexofficial@gmail.com>
2019-05-18 21:18:57 +0200

    > moved invalid params msg for !restrict to a better place

--- commands/moderation/restrict/restrict.js
+++ commands/moderation/restrict/restrict.js
@@ -10,24 +10,6 @@ module.exports = {
 
             let type = msg.content.split(" ")[1];
 
-            if (mentionedUsers.size < 1 && !type) {
-                msg.channel.send({
-                    embed: {
-                        "title": "Restrict command usage",
-                        "color": CONFIG.EMBED.COLORS.FAIL,
-                        "description": `
-                            You must mention someone to be restricted or restrict type.
-                        `,
-                        "footer": CONFIG.EMBED.FOOTER(handleData)
-                    }
-                }).then(()=>{
-                    return resolve(1);
-                }).catch((e)=>{
-                    return reject("Error sending 'no mentions' message: " + e);
-                });
-                return;
-            }
-
             dbInt.getGuildDoc(msg.guild.id).then((doc) => {
                 let restrictions = doc.restrictions;
 
@@ -47,6 +29,24 @@ module.exports = {
                 }
                 if (type == "clear") {
                     restrictions = false;
+                }
+
+                if (restrictions.length < 1) { // Empty array means no user was mentioned and there were no valid types of restriction in params to overwrite the empty array
+                    msg.channel.send({
+                        embed: {
+                            "title": "Restrict command usage",
+                            "color": CONFIG.EMBED.COLORS.FAIL,
+                            "description": `
+                                You must mention someone to be restricted or restrict type.
+                            `,
+                            "footer": CONFIG.EMBED.FOOTER(handleData)
+                        }
+                    }).then(()=>{
+                        return resolve(1);
+                    }).catch((e)=>{
+                        return reject("Error sending 'no mentions' message: " + e);
+                    });
+                    return;
                 }
 
                 doc.restrictions = restrictions;
