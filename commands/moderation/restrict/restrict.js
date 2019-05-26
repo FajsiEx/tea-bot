00576fae2b948e428c3723f32da0630e734ffa6f
FajsiEx <fajsiexofficial@gmail.com>
2019-05-26 16:30:30 +0200

    / fixed empty array response on dev restrict attempt

--- commands/moderation/restrict/restrict.js
+++ commands/moderation/restrict/restrict.js
@@ -15,32 +15,51 @@ module.exports = {
             dbInt.getGuildDoc(msg.guild.id).then(async (doc) => {
                 let restrictions = doc.restrictions;
 
-                if (typeof(restrictions) != "object") {
+                if (!type) { // If the command does not have an argument (!mod:restrict)
+                    await msg.channel.send({
+                        embed: {
+                            "title": "Restrict",
+                            "color": CONFIG.EMBED.COLORS.INFO,
+                            "description": `
+                                Current restrictions: ${restrictions}
+                            `,
+                            "footer": CONFIG.EMBED.FOOTER(handleData)
+                        }
+                    });
+                    return resolve(0);
+                }
+
+                if (typeof (restrictions) != "object") {
                     restrictions = [];
                 }
 
-                await (()=>{
-                    return new Promise((resolve, reject)=> {
-                        mentionedUsers.forEach(async (mentionedUser)=>{
-                            if (await permChecker.dev(mentionedUser.id)) {
-                                console.log("Nope");
-                                msg.channel.send({
-                                    embed: {
-                                        "title": "Restrict",
-                                        "color": CONFIG.EMBED.COLORS.FAIL,
-                                        "description": `
-                                            You really thought you can restrict my master? Haha le funny joke!\n\`Do that one more time and I'll break your fucking knees.\`
-                                        `,
-                                        "footer": CONFIG.EMBED.FOOTER(handleData)
-                                    }
-                                }).then((botMsg)=>{botMsg.delete(10000);});
-                            }else{
-                                restrictions.push(mentionedUser.id);
+                let triedToRestrictDev = false; // True = won't show empty mentions error
+
+                mentionedUsers.forEach(async (mentionedUser) => {
+                    if (await permChecker.dev(mentionedUser.id)) {
+                        console.log("Nope");
+                        msg.channel.send({
+                            embed: {
+                                "title": "Baka!",
+                                "color": CONFIG.EMBED.COLORS.FAIL,
+                                "description": `
+                                    You really thought you can restrict my master? Haha le funny joke!\n\`Do that one more time and I'll break your fucking knees.\`
+                                `,
+                                "footer": CONFIG.EMBED.FOOTER(handleData)
                             }
+                        }).then((botMsg) => {
+                            msg.delete(10000); // Delete the author's message
+                            botMsg.delete(10000); // and also this message.
                         });
-                    });
+
+                        triedToRestrictDev = true;
+                    } else {
+                        restrictions.push(mentionedUser.id);
+                    }
                 });
-                
+
+                await Promise.all(restrictions);
+
 
                 if (type == "dev") {
                     restrictions = "dev";
@@ -53,6 +72,7 @@ module.exports = {
                 }
 
                 if (restrictions.length < 1) { // Empty array means no user was mentioned and there were no valid types of restriction in params to overwrite the empty array
+                    if (triedToRestrictDev) {return resolve(1);}
                     msg.channel.send({
                         embed: {
                             "title": "Restrict command usage",
@@ -62,9 +82,9 @@ module.exports = {
                             `,
                             "footer": CONFIG.EMBED.FOOTER(handleData)
                         }
-                    }).then(()=>{
+                    }).then(() => {
                         return resolve(1);
-                    }).catch((e)=>{
+                    }).catch((e) => {
                         return reject("Error sending 'no mentions' message: " + e);
                     });
                     return;
@@ -82,9 +102,9 @@ module.exports = {
                             `,
                             "footer": CONFIG.EMBED.FOOTER(handleData)
                         }
-                    }).then(()=>{
+                    }).then(() => {
                         return resolve(0);
-                    }).catch((e)=>{
+                    }).catch((e) => {
                         return reject("Error sending result message: " + e);
                     });
                 });
