9f11a1c2279ca867160033d9d2a81c9dee185dad
FajsiEx <fajsiexofficial@gmail.com>
2019-06-09 21:52:49 +0200

    > converted bridge get guild doc to async syntax

--- db/bridge.js
+++ db/bridge.js
@@ -4,38 +4,48 @@ const MongoClient = require('mongodb').MongoClient;
 const cache = require("./cache");
 
 module.exports = {
-    // Gets document of the guild data from guild collection. If it does not exist it will call createGuildDocument and return the freshly created document
-    getGuildDocument: function(guildId) {
-        return new Promise((resolve)=>{
+// Gets document of the guild data from guild collection. If it does not exist it will call createGuildDocument and return the freshly created document
+getGuildDocument: async function (guildId) {
+    let client;
+    try {
+        client = await MongoClient.connect(DB_URI);
+    } // Connect to Wanilla mongoDB
+    catch (e) {
+        throw ("Failed to connect to db: " + e);
+    }
+
+    let db = client.db('tea-bot'); // Get tea-bot db
+    let docs;
+    try { docs = await db.collection("guilds").find({guildId: guildId}).toArray(); } // Find document in the guild collection by guildId and convert that to an array
+    catch (e) { throw ("Could not get docs from collection: " + e); }
+
+    if (docs.length < 1) { // If the array length is less than one (0) - the doc doesn't exist
+        let doc;
+        try { doc = await this.createGuildDocument(guildId); }
+        catch (e) { throw ("Could not create guildDoc " + e); }
+
+        cache.setCache(guildId, doc); // store the doc in cache
+        
+        client.close();
+        return doc; // and return that newly created doc.
+    } else { // If the guild doc exists
+        cache.setCache(guildId, docs[0]); // store the doc in cache
+
+        client.close();
+        return docs[0]; // and return that.
+    }
+},
+
+// Creates a doc in the guilds collection based on guildId
+createGuildDocument: function (guildId) {
+        return new Promise((resolve) => {
             MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
                 if (err) return console.error(err); // If there's a problem, return.
 
                 let db = client.db('tea-bot'); // Get tea-bot db
-                db.collection("guilds").find({guildId: guildId}).toArray((err, docs)=> { // Find document in the guild collection by guildId and convert that to an array
-                    if (err) return console.error(err); // If there's a problem, return.
-
-                    if (docs.length < 1) { // If the array length is less than one (0) the doc doesn't exist
-                        this.createGuildDocument(guildId).then((doc)=>{ // so create it
-                            cache.setCache(guildId, doc); // store the doc in cache
-                            resolve(doc); // and return that newly created doc.
-                        });
-                    }else{ // If the guild doc exist
-                        cache.setCache(guildId, docs[0]); // store the doc in cache
-                        resolve(docs[0]); // and return that.
-                    }
-                });
-            });
-        });
-    },
-
-    // Creates a doc in the guilds collection based on guildId
-    createGuildDocument: function(guildId) {
-        return new Promise((resolve)=>{
-            MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
-                if (err) return console.error(err); // If there's a problem, return.
-
-                let db = client.db('tea-bot'); // Get tea-bot db
-                db.collection("guilds").insertOne({guildId: guildId}, (err, res)=> { // Insert doc with guildId to the guilds collection
+                db.collection("guilds").insertOne({
+                    guildId: guildId
+                }, (err, res) => { // Insert doc with guildId to the guilds collection
                     if (err) return console.error(err); // If there's a problem, return.
 
                     resolve(res.ops[0]); // Return the new guild doc
@@ -45,13 +55,15 @@ module.exports = {
     },
 
     // Creates a doc in the guilds collection based on guildId
-    deleteGuildDocument: function(guildId) {
-        return new Promise((resolve)=>{
+    deleteGuildDocument: function (guildId) {
+        return new Promise((resolve) => {
             MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
                 if (err) return console.error(err); // If there's a problem, return.
 
                 let db = client.db('tea-bot'); // Get tea-bot db
-                db.collection("guilds").deleteOne({guildId: guildId}, (err)=> { // Delete doc with guildId to the guilds collection
+                db.collection("guilds").deleteOne({
+                    guildId: guildId
+                }, (err) => { // Delete doc with guildId to the guilds collection
                     if (err) return console.error(err); // If there's a problem, return.
 
                     console.log(`[DB:DELETEGD] WARNING Deleted guild document [${guildId}]!`.warn);
@@ -64,15 +76,19 @@ module.exports = {
         });
     },
 
-    writeGuildDocument: function(guildId, guildDoc) {
-        return new Promise((resolve)=>{
+    writeGuildDocument: function (guildId, guildDoc) {
+        return new Promise((resolve) => {
             cache.setCache(guildId, guildDoc); // store the doc in cache
 
             MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
                 if (err) return console.error(err); // If there's a problem, return.
 
                 let db = client.db('tea-bot'); // Get tea-bot db
-                db.collection("guilds").updateOne({guildId: guildId}, {$set:guildDoc}, (err)=> { // Update doc with guildId
+                db.collection("guilds").updateOne({
+                    guildId: guildId
+                }, {
+                    $set: guildDoc
+                }, (err) => { // Update doc with guildId
                     if (err) return console.error(err); // If there's a problem, return.
                     resolve(true);
                 });
@@ -80,14 +96,14 @@ module.exports = {
         });
     },
 
-    createStickyMsgDocument: function(documentData) {
-        return new Promise((resolve, reject)=>{
+    createStickyMsgDocument: function (documentData) {
+        return new Promise((resolve, reject) => {
             // TODO: add checks plz
             MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
                 if (err) reject("Connection error " + err); // If there's a problem, return.
 
                 let db = client.db('tea-bot'); // Get tea-bot db
-                db.collection("sticky").insertOne(documentData, (err, res)=> { // Insert doc with the data of the sticky message to "sticky" collection
+                db.collection("sticky").insertOne(documentData, (err, res) => { // Insert doc with the data of the sticky message to "sticky" collection
                     if (err) reject("Insert error " + err); // If there's a problem, return.
 
                     resolve(res.ops[0]); // Return the new sticky msg doc
@@ -96,22 +112,33 @@ module.exports = {
         });
     },
 
-    getExpiredStickyDocs: function(guildId, forceUpdate) {
-        return new Promise((resolve, reject)=>{
+    getExpiredStickyDocs: function (guildId, forceUpdate) {
+        return new Promise((resolve, reject) => {
             MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
                 if (err) reject("Connection error " + err); // If there's a problem, return.
 
                 let db = client.db('tea-bot'); // Get tea-bot db
-                
-                let eventExpiryDeadline = new Date().getTime();
-                if (forceUpdate) {eventExpiryDeadline = Infinity;}
 
-                let query = {expiry: {$lte: eventExpiryDeadline}};
-                if (guildId) { // If guildId was specified
-                    query = {expiry: {$lte: eventExpiryDeadline}, g_id: guildId};
+                let eventExpiryDeadline = new Date().getTime();
+                if (forceUpdate) {
+                    eventExpiryDeadline = Infinity;
                 }
 
-                db.collection("sticky").find(query).toArray((err, docs)=> { // 
+                let query = {
+                    expiry: {
+                        $lte: eventExpiryDeadline
+                    }
+                };
+                if (guildId) { // If guildId was specified
+                    query = {
+                        expiry: {
+                            $lte: eventExpiryDeadline
+                        },
+                        g_id: guildId
+                    };
+                }
+
+                db.collection("sticky").find(query).toArray((err, docs) => { // 
                     if (err) reject("Connection error " + err); // If there's a problem, return.
                     resolve(docs);
                 });
@@ -119,13 +146,17 @@ module.exports = {
         });
     },
 
-    updateStickyDoc: function(m_id, stickyDocUpdateData) {
-        return new Promise((resolve, reject)=>{
+    updateStickyDoc: function (m_id, stickyDocUpdateData) {
+        return new Promise((resolve, reject) => {
             MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
                 if (err) reject("Connection error " + err); // If there's a problem, return.
 
                 let db = client.db('tea-bot'); // Get tea-bot db
-                db.collection("sticky").updateOne({m_id: m_id}, {$set:stickyDocUpdateData}, (err)=> { // Update doc with the m_id (message id of the sticky message)
+                db.collection("sticky").updateOne({
+                    m_id: m_id
+                }, {
+                    $set: stickyDocUpdateData
+                }, (err) => { // Update doc with the m_id (message id of the sticky message)
                     if (err) reject("Connection error " + err); // If there's a problem, return.
                     resolve(true);
                 });
@@ -133,14 +164,16 @@ module.exports = {
         });
     },
 
-    deleteStickyDoc: function(m_id) {
-        return new Promise((resolve)=>{
+    deleteStickyDoc: function (m_id) {
+        return new Promise((resolve) => {
             // TODO: Add check if m_id is a number
             MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
                 if (err) reject("Connection error " + err); // If there's a problem, return.
 
                 let db = client.db('tea-bot'); // Get tea-bot db
-                db.collection("sticky").deleteOne({m_id: m_id}, (err)=> { // Delete sticky doc based on m_id
+                db.collection("sticky").deleteOne({
+                    m_id: m_id
+                }, (err) => { // Delete sticky doc based on m_id
                     if (err) reject("Connection error " + err); // If there's a problem, return.
                     resolve(true); // Return with success
                 });
@@ -148,14 +181,16 @@ module.exports = {
         });
     },
 
-    deleteAllStickyDocsFromChannel: function(c_id) {
-        return new Promise((resolve)=>{
+    deleteAllStickyDocsFromChannel: function (c_id) {
+        return new Promise((resolve) => {
             // TODO: Add check if c_id is a number
             MongoClient.connect(DB_URI, (err, client) => { // Connect to Wanilla mongoDB
                 if (err) reject("Connection error " + err); // If there's a problem, return.
 
                 let db = client.db('tea-bot'); // Get tea-bot db
-                db.collection("sticky").deleteMany({c_id: c_id}, (err)=> { // Delete sticky doc based on m_id
+                db.collection("sticky").deleteMany({
+                    c_id: c_id
+                }, (err) => { // Delete sticky doc based on m_id
                     if (err) reject("Connection error " + err); // If there's a problem, return.
                     resolve(true); // Return with success
                 });
