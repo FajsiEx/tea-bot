73d73a90aa95071aa04a613a0659dec82042c6ea
FajsiEx <fajsiexofficial@gmail.com>
2019-06-13 20:29:01 +0200

    > fixed formatting of !ping

--- commands/ping.js
+++ commands/ping.js
@@ -2,26 +2,23 @@ const CONFIG = require("../modules/config");
 const outdent = require("outdent");
 
 module.exports = {
-  handler: handleData => {
-    return new Promise((resolve, reject) => {
-      let msg = handleData.msg;
-      msg.channel
-        .send({
-          embed: {
-            title: "Ping",
-            color: CONFIG.EMBED.COLORS.INFO,
-            description: outdent`
-              Server message latency: ${new Date().getTime() - msg.createdTimestamp} ms
-            `,
-            footer: CONFIG.EMBED.FOOTER(handleData)
-          }
-        })
-        .then(() => {
-          return resolve(0);
-        })
-        .catch(e => {
-          return reject("Failed sending message: " + e);
+    handler: handleData => {
+        return new Promise((resolve, reject) => {
+            let msg = handleData.msg;
+            msg.channel.send({
+                embed: {
+                    title: "Ping",
+                    color: CONFIG.EMBED.COLORS.INFO,
+                    description: outdent`
+                        Server message latency: ${new Date().getTime() - msg.createdTimestamp} ms
+                    `,
+                    footer: CONFIG.EMBED.FOOTER(handleData)
+                }
+            }).then(() => {
+                return resolve(0);
+            }).catch(e => {
+                return reject("Failed sending message: " + e);
+            });
         });
-    });
-  }
+    }
 };
