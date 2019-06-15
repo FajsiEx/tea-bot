4d2ac0adfa9eb7aeda525b5154d4f016b901c85f
FajsiEx <fajsiexofficial@gmail.com>
2019-06-15 19:58:01 +0200

    > added warning for missing sentry dsn

--- sentry/init.js
+++ sentry/init.js
@@ -10,6 +10,13 @@ const Sentry = require('@sentry/node');
 
 module.exports = {
     init: function() {
+        if (CONFIG.SECRETS.SENTRY.DSN) {
+            console.log("/!\\ WARNING /!\\".warn);
+            console.log("Sentry DSN url not provided - SENTRY ERROR TRACKING IS DISABLED!".warn);
+            console.log("If you're testing locally, you can ignore this error.".warn);
+            console.log("If this code is running in production, please provide your own sentry DSN url for error tracking.".warn);
+            return false;
+        }
         Sentry.init({ dsn: CONFIG.SECRETS.SENTRY.DSN });
     }
 };
