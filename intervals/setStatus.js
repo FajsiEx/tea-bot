541d37020f94529e5d7f2821d721653ed2fe392a
FajsiEx <fajsiexofficial@gmail.com>
2019-06-19 21:34:40 +0200

    * status the db ready

--- intervals/setStatus.js
+++ intervals/setStatus.js
@@ -1,13 +1,20 @@
-const CONFIG = require("../modules/config");
+const CONFIG = require("/modules/config");
+const dbBridge = require("/db/bridge");
 
 module.exports = {
-    interval: function(dClient) {
+    interval: function (dClient) {
         let activityString = `with ${dClient.guilds.size} guilds | ${CONFIG.BOT.BUILD_INFO.BUILD_STRING} | ${module.exports.getTimeString()}`;
 
         dClient.user.setActivity(activityString, { type: "PLAYING" });
+
+        if (!dbBridge.isDBReady()) {
+            dClient.user.setStatus("idle");
+        } else {
+            dClient.user.setStatus("online");
+        }
     },
-    
-    getTimeString: function() {
+
+    getTimeString: function () {
         let dt = new Date();
         let hours = (dt.getHours() < 10 ? '0' : '') + dt.getHours();
         let minutes = (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
