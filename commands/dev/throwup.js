d7aadf4d4d6296effc6effb5b5adf5045dbc407d
FajsiEx <fajsiexofficial@gmail.com>
2019-06-20 21:35:58 +0200

    > unrandomized the throwup reject

--- commands/dev/throwup.js
+++ commands/dev/throwup.js
@@ -6,8 +6,6 @@
 
 module.exports = {
     handler: async function(handleData) {
-        let andTestingTimes = (Math.random()*100) % 32;
-
-        throw(`Intentional reject. This command module is meant for error tracking testing${" and testing".repeat(andTestingTimes)}.`);
+        throw(`Intentional reject. おまえは もう しんでいる`);
     }
 };
