dcb906f62f07968628859f701b790cef28f2eb81
FajsiEx <fajsiexofficial@gmail.com>
2019-06-23 21:03:21 +0200

    + checks for incoming trigger data

--- socket/triggers.js
+++ socket/triggers.js
@@ -7,6 +7,13 @@
 
 module.exports = {
     incomingData: function(data) {
-        console.log(data);
+        console.log(module.exports.checkIncomingData(data));
+    },
+
+    checkIncomingData: function(data) {
+        if (!data.token) {return false;}
+        if (!data.body) {return false;}
+
+        return true;
     }
 };
