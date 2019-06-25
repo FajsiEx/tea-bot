aded3c6947501692a0320bca9332337ba4b0966f
FajsiEx <fajsiexofficial@gmail.com>
2019-06-25 17:41:01 +0200

    > asynced !invalid:category

--- commands/invalid/category.js
+++ commands/invalid/category.js
@@ -1,24 +1,25 @@
 const CONFIG = require("../../modules/config");
 
 module.exports = {
-    handler: (handleData) => {
-        return new Promise((resolve, reject) => {
-            let msg = handleData.msg;
-            msg.channel.send({
+    handler: async function (messageEventData) {
+        let msg = messageEventData.msg;
+
+        try {
+            let botMsg = await msg.channel.send({
                 embed: {
                     "title": "Invalid command category",
                     "color": CONFIG.EMBED.COLORS.FAIL,
                     "description": `
                         Look at the docs for valid command categories and their commands
                     `,
-                    "footer": CONFIG.EMBED.FOOTER(handleData)
+                    "footer": CONFIG.EMBED.FOOTER(messageEventData)
                 }
-            }).then((botMsg) => {
-                botMsg.delete(15000);
-                resolve(0);
-            }).catch((e)=>{
-                return reject("Failed to send invalid category message: " + e);
             });
-        }); // End of promise
-    } // End of handler
+            botMsg.delete(15000);
+        } catch (e) {
+            throw("Failed to send invalid command message: " + e);
+        }
+
+        return 0;
+    }
 };
