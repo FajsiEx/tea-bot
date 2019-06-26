5f57b57a1d45db5ad8d923634c7f81036697c11b
FajsiEx <fajsiexofficial@gmail.com>
2019-06-26 16:55:57 +0200

    > asynced !mod:restrict #8 #37

--- commands/moderation/restrict/restrict.js
+++ commands/moderation/restrict/restrict.js
@@ -4,110 +4,138 @@ const dbInt = require("../../../db/interface");
 const permChecker = require("../../../modules/permChecker");
 
 module.exports = {
-    handler: (handleData) => {
-        return new Promise(async (resolve, reject) => {
-            let msg = handleData.msg;
+    handler: async function (messageEventData) {
+        let msg = messageEventData.msg;
 
-            let mentionedUsers = msg.mentions.users;
+        let mentionedUsers = msg.mentions.users;
 
-            let type = msg.content.split(" ")[1];
+        let type = msg.content.split(" ")[1];
 
-            dbInt.getGuildDoc(msg.guild.id).then(async (doc) => {
-                let restrictions = doc.restrictions;
+        let doc;
 
-                if (!type) { // If the command does not have an argument (!mod:restrict)
-                    await msg.channel.send({
-                        embed: {
-                            "title": "Restrict",
-                            "color": CONFIG.EMBED.COLORS.INFO,
-                            "description": `
-                                Current restrictions: ${restrictions}
-                            `,
-                            "footer": CONFIG.EMBED.FOOTER(handleData)
-                        }
-                    });
-                    return resolve(0);
-                }
+        try {
+            await dbInt.getGuildDoc(msg.guild.id);
+        } catch (e) {
+            throw ("Failed to get guild doc: " + e);
+        }
 
-                if (typeof (restrictions) != "object") {
-                    restrictions = [];
-                }
+        let restrictions = doc.restrictions;
 
-                let triedToRestrictDev = false; // True = won't show empty mentions error
-
-                mentionedUsers.forEach(async (mentionedUser) => {
-                    if (await permChecker.dev(mentionedUser.id)) {
-                        msg.channel.send({
-                            embed: {
-                                "title": "Baka!",
-                                "color": CONFIG.EMBED.COLORS.FAIL,
-                                "description": `
-                                    You really thought you can restrict my master? Haha le funny joke!\n\`Do that one more time and I'll break your fucking knees.\`
-                                `,
-                                "footer": CONFIG.EMBED.FOOTER(handleData)
-                            }
-                        }).then((botMsg) => {
-                            msg.delete(10000); // Delete the author's message
-                            botMsg.delete(10000); // and also this message.
-                        });
-
-                        triedToRestrictDev = true;
-                    } else {
-                        restrictions.push(mentionedUser.id);
+        if (!type) { // If the command does not have an argument (only !mod:restrict)
+            try { // We will send listing of restrictions
+                await msg.channel.send({ // TODO: Make this do more instead just stringifying data
+                    embed: {
+                        "title": "Restrict",
+                        "color": CONFIG.EMBED.COLORS.INFO,
+                        "description": `
+                            Current restrictions: ${restrictions}
+                        `,
+                        "footer": CONFIG.EMBED.FOOTER(messageEventData)
                     }
                 });
+            } catch (e) {
+                throw ("Failed to send restrict listing msg: " + e);
+            }
+            return 0;
+        }
 
-                await Promise.all(restrictions);
+        if (typeof (restrictions) != "object") {
+            restrictions = []; // Set restrictions as an array to begin with
+        }
 
+        let triedToRestrictDev = false; // True = won't show empty mentions error
 
-                if (type == "dev") {
-                    restrictions = "dev";
-                }
-                if (type == "admin") {
-                    restrictions = "admin";
-                }
-                if (type == "clear") {
-                    restrictions = false;
-                }
+        mentionedUsers.forEach(async function (mentionedUser) {
+            let isDev;
+            try { isDev = await permChecker.dev(mentionedUser.id); }
+            catch (e) { throw ("Failed to check dev perm:" + e); }
 
-                if (restrictions.length < 1) { // Empty array means no user was mentioned and there were no valid types of restriction in params to overwrite the empty array
-                    if (triedToRestrictDev) {return resolve(1);}
-                    msg.channel.send({
+            if (isDev) {
+                let botMsg;
+
+                try {
+                    botMsg = msg.channel.send({
                         embed: {
-                            "title": "Restrict command usage",
+                            "title": "Baka!",
                             "color": CONFIG.EMBED.COLORS.FAIL,
                             "description": `
-                                You must mention someone to be restricted or restrict type.
+                                You really thought you can restrict my master? wwwww\n\`Do that one more time and I'll break your fucking knees :)\`
                             `,
-                            "footer": CONFIG.EMBED.FOOTER(handleData)
+                            "footer": CONFIG.EMBED.FOOTER(messageEventData)
                         }
-                    }).then(() => {
-                        return resolve(1);
-                    }).catch((e) => {
-                        return reject("Error sending 'no mentions' message: " + e);
                     });
-                    return;
+                } catch (e) {
+                    throw ("Failed to send a fail message: " + e);
                 }
 
-                doc.restrictions = restrictions;
+                try {
+                    msg.delete(10000); // Delete the author's message
+                    botMsg.delete(10000); // and also this message.
+                } catch (e) {
+                    throw ("Failed to delete: " + e);
+                }
 
-                dbInt.setGuildDoc(msg.guild.id, doc).then(() => {
-                    msg.channel.send({
-                        embed: {
-                            "title": "Restrict",
-                            "color": CONFIG.EMBED.COLORS.SUCCESS,
-                            "description": `
-                                Done.
-                            `,
-                            "footer": CONFIG.EMBED.FOOTER(handleData)
-                        }
-                    }).then(() => {
-                        return resolve(0);
-                    }).catch((e) => {
-                        return reject("Error sending result message: " + e);
-                    });
-                });
-            });
+                triedToRestrictDev = true;
+            } else {
+                restrictions.push(mentionedUser.id);
+            }
         });
+
+        await Promise.all(restrictions);
+
+
+        if (type == "dev") {
+            restrictions = "dev";
+        }
+        if (type == "admin") {
+            restrictions = "admin";
+        }
+        if (type == "clear") {
+            restrictions = false;
+        }
+
+        if (restrictions.length < 1) { // Empty array means no user was mentioned and there were no valid types of restriction in params to overwrite the empty array
+            if (triedToRestrictDev) { return 1; } // If user tried to restrict dev, and did not mention any user other than dev
+
+            try {
+                await msg.channel.send({
+                    embed: {
+                        "title": "Restrict command usage",
+                        "color": CONFIG.EMBED.COLORS.FAIL,
+                        "description": `
+                        You must mention someone to be restricted or enter a restrict type.
+                    `,
+                        "footer": CONFIG.EMBED.FOOTER(messageEventData)
+                    }
+                });
+            } catch (e) {
+                throw ("Error sending 'no mentions' message: " + e);
+            }
+
+            return 1;
+        }
+
+        doc.restrictions = restrictions;
+
+        try {
+            await dbInt.setGuildDoc(msg.guild.id, doc);
+        } catch (e) {
+            throw ("Error sending result message: " + e);
+        }
+
+        try {
+            msg.channel.send({
+                embed: {
+                    "title": "Restrict",
+                    "color": CONFIG.EMBED.COLORS.SUCCESS,
+                    "description": `
+                        Done.
+                    `,
+                    "footer": CONFIG.EMBED.FOOTER(messageEventData)
+                }
+            });
+        } catch (e) {
+            throw ("Failed to send success message: " + e);
+        }
     }
 };
