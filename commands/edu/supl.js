204d3455e43a84be971ad67ec38ccfe7e2eeaee4
FajsiEx <fajsiexofficial@gmail.com>
2019-06-30 12:22:02 +0200

    + a lot of scraping of edupage subs

--- commands/edu/supl.js
+++ commands/edu/supl.js
@@ -21,18 +21,46 @@ module.exports = {
             "Referrer":"https://spseadlerka.edupage.org/substitution/",
             "Cookie": "PHPSESSID=e87c3777f56ab84171ac669bd120aed8; hsid=25f2aa8a544afec02735208d750413e2ceb5f81d"
 
-        },"referrer":"https://spseadlerka.edupage.org/substitution/","referrerPolicy":"no-referrer-when-downgrade","body":"{\"__args\":[null,{\"date\":\"2019-06-14\",\"mode\":\"classes\"}],\"__gsh\":\"4027fe5c\"}","method":"POST","mode":"cors"}).then((d)=>{return d.text()})
+        },"referrer":"https://spseadlerka.edupage.org/substitution/","referrerPolicy":"no-referrer-when-downgrade","body":"{\"__args\":[null,{\"date\":\"2019-06-13\",\"mode\":\"classes\"}],\"__gsh\":\"4027fe5c\"}","method":"POST","mode":"cors"}).then((d)=>{return d.text()})
 
         let document = htmlParser.parse(JSON.parse(tempCont).r, "text/html");
         console.log(document);
+        console.log(document.innerHTML);
+
+        let subNote = document.querySelector("div div div span").innerHTML;
+        let subNoteTitle = subNote.split(":")[0]
+        let subNoteContent = (subNote.split(":")[1]) ? subNote.split(":")[1].split(",").join("\n") : "";
+
+        let subSects = document.querySelectorAll(".section");
+        console.log(subSects);
+
+        let classSubString;
+        subSects.forEach(section=>{
+            let className = section.querySelector(".header span").innerHTML;
+            if (className != "I.B") return;
+
+            classSubString = `**${className}**\n`;
+            
+            let rows = section.querySelectorAll(".row");
+
+            rows.forEach(row=>{
+                let period = row.querySelector(".period span").innerHTML;
+                let info = row.querySelector(".info span").innerHTML;
+
+                classSubString += `${period} - ${info}\n\n`;
+            });
+        });
 
         try {
             await msg.channel.send({
                 embed: {
                     title: "Edu | Substitution",
                     color: CONFIG.EMBED.COLORS.INFO,
-                    description: `
-                        Logged in console
+                    description: outdent`
+                        **${subNoteTitle}**
+                        ${subNoteContent}
+
+                        ${classSubString}
                     `,
                     footer: CONFIG.EMBED.FOOTER(handleData)
                 }
