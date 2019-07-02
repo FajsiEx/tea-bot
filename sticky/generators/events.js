408f51ac153388e4002b3627249591aa829f6234
FajsiEx <fajsiexofficial@gmail.com>
2019-07-02 14:37:16 +0200

    + sorting into categories

--- sticky/generators/events.js
+++ sticky/generators/events.js
@@ -1,33 +1,6 @@
 const CONFIG = require("../../modules/config");
 const dbInt = require("../../db/interface");
 
-const categories = [
-    {
-        name: "In the next 24 hours",
-        maxDelta: 24*60*60*1000
-    },
-    {
-        name: "In the next 48 hours",
-        maxDelta: 48*60*60*1000
-    },
-    {
-        name: "In the next week",
-        maxDelta: 7*24*60*60*1000
-    },
-    {
-        name: "In the next month",
-        maxDelta: 30*24*60*60*1000
-    },
-    {
-        name: "In the next year",
-        maxDelta: 365.25*24*60*60*1000
-    },
-    {
-        name: "Other",
-        maxDelta: Infinity
-    },
-];
-
 module.exports = {
     generator: async function (generatorData) {
         let guildId = generatorData.guildId;
@@ -50,6 +23,54 @@ module.exports = {
         }
 
         eventsArray.sort(function(a, b){return a.date.getTime() - b.date.getTime();});
+        
+
+        //* CATEGORIES
+        let categories = [
+            {
+                name: "In the next 24 hours",
+                maxDelta: 24*60*60*1000,
+                events: []
+            },
+            {
+                name: "In the next 48 hours",
+                maxDelta: 48*60*60*1000,
+                events: []
+            },
+            {
+                name: "In the next week",
+                maxDelta: 7*24*60*60*1000,
+                events: []
+            },
+            {
+                name: "In the next month",
+                maxDelta: 30*24*60*60*1000,
+                events: []
+            },
+            {
+                name: "In the next year",
+                maxDelta: 365.25*24*60*60*1000,
+                events: []
+            },
+            {
+                name: "Other",
+                maxDelta: Infinity,
+                events: []
+            },
+        ];
+
+        const currentTime = new Date().getTime();
+
+        for (let event of eventsArray) {
+            if (event.date.getTime() < new Date().getTime()) { continue; } // If the event is expired, move on
+
+            for(let category of categories) {
+                if (event.date.getTime() < currentTime + category.maxDelta) {
+                    category.events.push(event);
+                    break;
+                }
+            }
+        }
 
         let resultEventString = "";
         eventsArray.forEach((event) => {
