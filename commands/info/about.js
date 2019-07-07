49912c00025ef7f8b6ccb1ab590ad52edf2e62f8
FajsiEx <fajsiexofficial@gmail.com>
2019-07-07 22:42:47 +0200

    + added splash strings to !about

--- commands/info/about.js
+++ commands/info/about.js
@@ -10,7 +10,8 @@ module.exports = {
                     "title": "Tea-bot | About",
                     "color": CONFIG.EMBED.COLORS.INFO,
                     "description": outdent`
-                        **「 Tea-bot Re:Write 」** *Starting code in a better way from zero*
+                        **「 Tea-bot Re:Write 」 Project **
+                        *${CONFIG.SPLASH_STRINGS.GET()}*
                         
                         Build **${CONFIG.BOT.BUILD_INFO.BUILD_STRING}**
                         Server message latency: **${new Date().getTime() - msg.createdTimestamp} ms**
