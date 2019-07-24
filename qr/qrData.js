a04bc96f7cb273c656d96980dedc9d96860e932a
DanValnicek <danvalnicek@gmail.com>
2019-07-24 11:43:20 +0200

    !aysat

--- qr/qrData.js
+++ qr/qrData.js
@@ -161,4 +161,9 @@ module.exports = [
         type: "file",
         data: "https://i.imgur.com/Dv9li1r.jpg"
     },
+    {
+        keywords: ["aysat", "areyousure","sure","areyousureaboutthat"],
+        type: "file",
+        data: "https://tenor.com/view/sure-john-cena-gif-5783187"
+    }
 ];
