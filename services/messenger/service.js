ffae83e4faf1a21ad13dc29ca451f87143948bd0
FajsiEx <fajsiexofficial@gmail.com>
2019-09-06 22:44:16 +0200

    + basic mess service listeners

--- services/messenger/service.js
+++ services/messenger/service.js
@@ -1,8 +1,20 @@
+const fbMessenger = require("facebook-chat-api");
+const dbBridge = require("../../db/bridge");
 
 module.exports.init = function() {
-    
+    fbMessenger({email: process.env.T_FB_USER, password: process.env.T_FB_PASS, forceLogin: true}, (err, api)=>{
+        if (err) return console.log(err);
+
+        api.listen((err, msg)=> {
+            console.log("[SERVICE: MESSENGER] Message event", msg);
+        });
+    });
 };
 
 module.exports.messageEventHandler = function(msg) {
+    
+};
+
+module.exports.bridgingHandler = function(msg) {
 
 };
