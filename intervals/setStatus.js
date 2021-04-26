649c3a01b88cf6d1c421bb55330bab865304f75d
FajsiEx <fajsiexofficial@gmail.com>
2021-04-26 13:09:01 +0200

    * disble

--- intervals/setStatus.js
+++ intervals/setStatus.js
@@ -13,28 +13,28 @@ module.exports = {
 
         dClient.user.setActivity(activityString, { type: "PLAYING" });
 
-        const today = new Date();
-        const maturaGoal = new Date("03/15/2022");
-        const maturaDiff = maturaGoal.getTime() - today.getTime();
-        let maturaDays = Math.floor(maturaDiff / (1000 * 60 * 60 * 24));
-        const maturaMonths = Math.floor(maturaDays / 30);
-        maturaDays -= maturaMonths * 30;
+        // const today = new Date();
+        // const maturaGoal = new Date("03/15/2022");
+        // const maturaDiff = maturaGoal.getTime() - today.getTime();
+        // let maturaDays = Math.floor(maturaDiff / (1000 * 60 * 60 * 24));
+        // const maturaMonths = Math.floor(maturaDays / 30);
+        // maturaDays -= maturaMonths * 30;
 
-        const holidayGoal = new Date("06/30/2021");
-        const holidayDiff = holidayGoal.getTime() - today.getTime();
-        let holidayDays = Math.floor(holidayDiff / (1000 * 60 * 60 * 24));
-        const holidayMonths = Math.floor(holidayDays / 30);
-        holidayDays -= holidayMonths * 30;
+        // const holidayGoal = new Date("06/30/2021");
+        // const holidayDiff = holidayGoal.getTime() - today.getTime();
+        // let holidayDays = Math.floor(holidayDiff / (1000 * 60 * 60 * 24));
+        // const holidayMonths = Math.floor(holidayDays / 30);
+        // holidayDays -= holidayMonths * 30;
 
-        dClient.channels.fetch("829451949664763904");
-        dClient.channels.fetch("829755982808350750");
+        // dClient.channels.fetch("829451949664763904");
+        // dClient.channels.fetch("829755982808350750");
 
-        dClient.channels
-            .get("829451949664763904")
-            .setName(`🚨 Matura za: ${maturaMonths}mes ${maturaDays}dni`);
-        dClient.channels
-            .get("829755982808350750")
-            .setName(`🎉 Prazdn za: ${holidayMonths}mes ${holidayDays}dni`);
+        // dClient.channels
+        //     .get("829451949664763904")
+        //     .setName(`🚨 Matura za: ${maturaMonths}mes ${maturaDays}dni`);
+        // dClient.channels
+        //     .get("829755982808350750")
+        //     .setName(`🎉 Prazdn za: ${holidayMonths}mes ${holidayDays}dni`);
     },
 
     setup: function (dClient) {
